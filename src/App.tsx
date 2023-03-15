a48feec8f1bce1167599e1927c8c39975c9b48dc
412 <85489519+laybacksound96@users.noreply.github.com>
2023-03-15 21:12:05 +0900

    Slider (part1)

--- src/App.tsx
+++ src/App.tsx
@@ -9,49 +9,49 @@ const Wrapper = styled(motion.div)`
   justify-content: center;
   align-items: center;
   background: linear-gradient(135deg, rgb(238, 0, 153), rgb(221, 0, 238));
+  flex-direction: column;
 `;
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-
+  position: absolute;
+  top: 100px;
   background-color: rgb(255, 255, 255);
   border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 28px;
 `;
 
 const boxVariants = {
-  initial: {
-    opacity: 0,
-    scale: 0,
-  },
-  visible: {
-    opacity: 1,
-    scale: 1,
-    rotateZ: 360,
-  },
-  leaving: {
-    opacity: 0,
-    scale: 0,
-    y: 20,
-  },
+  invisible: { x: 500, opacity: 0, scale: 0 },
+  visible: { x: 0, opacity: 1, scale: 1 },
+  exit: { x: -500, opacity: 0, scale: 0 },
 };
 
 function App() {
-  const [isShowing, setIsShowing] = useState(false);
-  const toggleShowing = () => setIsShowing((prev) => !prev);
+  const [visible, setVisible] = useState(1);
+  const nextPlease = () => setVisible((prev) => (prev === 10 ? 10 : prev + 1));
   return (
     <Wrapper>
-      <button onClick={toggleShowing}>Click me</button>
       <AnimatePresence>
-        {isShowing ? (
-          <Box
-            variants={boxVariants}
-            initial="initial"
-            animate="visible"
-            exit="leaving"
-          ></Box>
-        ) : null}
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) =>
+          visible === i ? (
+            <Box
+              variants={boxVariants}
+              initial="invisible"
+              animate="visible"
+              exit="exit"
+              key={i}
+            >
+              {i}
+            </Box>
+          ) : null
+        )}
       </AnimatePresence>
+      <button onClick={nextPlease}>Next</button>
     </Wrapper>
   );
 }
